3c2f9a44e88fc650472859f58b274f3a49b338b8
tskrjanc <144027074+tskrjanc@users.noreply.github.com>
2024-09-18 10:09:58 +0200

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -38,6 +38,15 @@ def force_between_planets(position1, mass1, position2, mass2):
 
     return force
 
+def test_force_between_planets():
+    position1 = np.array([0.0, 0.0, 0.0])
+    mass1 = 1.0
+    position2 = np.array([1.0, 0.0, 0.0])
+    mass2 = 2.0
+
+    force = force_between_planets(position1, mass1, position2, mass2)
+
+    assert np.allclose(force, [2.0, 0.0, 0.0])
 
 def calculate_forces_range(args):
     start, end, positions, masses = args
